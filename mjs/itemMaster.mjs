8514d55830ad9ce501deb9f03a286a64f50ead4b
Eunomiac <ryan.o.west@gmail.com>
2021-09-19 12:08:20 -0400

    light updates from SVG; adding new maps soon

--- mjs/itemMaster.mjs
+++ mjs/itemMaster.mjs
@@ -5,10 +5,10 @@ import {alienrpgItemSheet} from "../../../systems/alienrpg/module/item/item-shee
 
 // #region ████████ SHEET OVERRIDES: Overriding Item Sheet with Subclass Extension ████████ ~
 const sheetTemplates = {
-    item: "modules/alienrpgoverrides/html/item/item-sheet.html"
+  item: "modules/alienrpgoverrides/html/item/item-sheet.html"
 };
 class alienrpgoverridesItemSheet extends alienrpgItemSheet {
-    get template() { return sheetTemplates.item }
+  get template() { return sheetTemplates.item }
 }
 // #endregion ▄▄▄▄▄ SHEET OVERRIDES ▄▄▄▄▄
 
@@ -17,18 +17,18 @@ const templates = Object.values(sheetTemplates);
 export {templates};
 
 export default (() => ({
-    "~setup": () => {
-        console.log("██████ SETTING UP ITEM SHEET OVERRIDES ... ██████");
-        Items.unregisterSheet("alienrpg", alienrpgItemSheet);
-        Items.registerSheet("alienrpg", alienrpgItemSheet, {
-            types: ["weapon", "armor", "talent", "skill-stunts", "agenda", "specialty"],
-            makeDefault: false
-        });
-        Items.registerSheet("alienrpg", alienrpgoverridesItemSheet, {
-            types: ["item"],
-            makeDefault: true
-        });
-        console.log("██████ ITEM SHEET OVERRIDING COMPLETE █████████");
-    }
+  "~setup": () => {
+    console.log("██████ SETTING UP ITEM SHEET OVERRIDES ... ██████");
+    Items.unregisterSheet("alienrpg", alienrpgItemSheet);
+    Items.registerSheet("alienrpg", alienrpgItemSheet, {
+      types: ["weapon", "armor", "talent", "skill-stunts", "agenda", "specialty"],
+      makeDefault: false
+    });
+    Items.registerSheet("alienrpg", alienrpgoverridesItemSheet, {
+      types: ["item"],
+      makeDefault: true
+    });
+    console.log("██████ ITEM SHEET OVERRIDING COMPLETE █████████");
+  }
 }))();
 // #endregion ▮▮▮▮[EXPORTS]▮▮▮▮
--- mjs/lightMaster.mjs
+++ mjs/lightMaster.mjs
@@ -1,84 +1,122 @@
 import {RE} from "./utils.mjs";
 
 const parseLightAdjustmentData = (light, upData) => {
-    const updateData = {...upData};
-    if (light) {
-        for (const [key, val] of Object.entries({...upData})) {
-            if (/%$/.test(key)) {
-                const realKey = key.slice(0, -1);
-                const curValue = light.data[realKey];
-                const newValue = curValue * val;
-                updateData[realKey] = newValue;
-                delete updateData[key];
-            }
-        }
+  const updateData = {...upData};
+  if (light) {
+    for (const [key, val] of Object.entries({...upData})) {
+      if (/%$/.test(key)) {
+        const realKey = key.slice(0, -1);
+        const curValue = light.data[realKey];
+        const newValue = curValue * val;
+        updateData[realKey] = newValue;
+        delete updateData[key];
+      }
     }
-    return updateData;
+  }
+  return updateData;
 };
 
 const parseLightUpdateData = (groupRef, upData) => {
-    const updateData = [];
-    if (typeof groupRef === "object") {
-        for (const [gRef, uData] of Object.entries(groupRef)) {
-            updateData.push(...parseLightUpdateData(gRef, uData));
-        }
-    } else {
-        const regExpTest = new RegExp(groupRef, "ui");
-        updateData.push(...canvas.scene.lights.filter((light) => regExpTest.test(light.data.flags?.alienrpgoverrides?.group))
-            .map((light) => ({_id: light.id, ...parseLightAdjustmentData(light, upData)})));
+  const updateData = [];
+  if (typeof groupRef === "object") {
+    for (const [gRef, uData] of Object.entries(groupRef)) {
+      updateData.push(...parseLightUpdateData(gRef, uData));
     }
-    return updateData;
+  } else {
+    const regExpTest = new RegExp(groupRef, "ui");
+    updateData.push(...canvas.scene.lights.filter((light) => regExpTest.test(light.data.flags?.alienrpgoverrides?.group))
+      .map((light) => ({_id: light.id, ...parseLightAdjustmentData(light, upData)})));
+  }
+  return updateData;
 };
 
 const parseSoundUpdateData = (soundName, upData) => {
-    const updateData = [];
-    if (typeof soundName === "object") {
-        for (const [sName, uData] of Object.entries(soundName)) {
-            updateData.push(...parseSoundUpdateData(sName, uData));
-        }
-    } else {
-        const regExpTest = new RegExp(soundName, "ui");
-        updateData.push(...canvas.scene.sounds.filter((sound) => regExpTest.test(sound.data.path))
-            .map((sound) => ({_id: sound.id, ...upData})));
+  const updateData = [];
+  if (typeof soundName === "object") {
+    for (const [sName, uData] of Object.entries(soundName)) {
+      updateData.push(...parseSoundUpdateData(sName, uData));
     }
-    return updateData;
+  } else {
+    const regExpTest = new RegExp(soundName, "ui");
+    updateData.push(...canvas.scene.sounds.filter((sound) => regExpTest.test(sound.data.path))
+      .map((sound) => ({_id: sound.id, ...upData})));
+  }
+  return updateData;
 };
 
 export default (() => ({
-    toggleLights: async (groupRef, isActive) => {
-        if (game.user.isGM) {
-            const updateData = [];
-            if (typeof groupRef === "object") {
-                for (const [gRef, iActive] of Object.entries(groupRef)) {
-                    groupRef[gRef] = {hidden: !iActive};
-                }
-                updateData.push(...parseLightUpdateData(groupRef));
-            } else {
-                updateData.push(...parseLightUpdateData(groupRef, {hidden: !isActive}));
-            }
-            return canvas.scene.updateEmbeddedDocuments("AmbientLight", updateData);
+  toggleLights: async (groupRef, isActive) => {
+    if (game.user.isGM) {
+      const updateData = [];
+      if (typeof groupRef === "object") {
+        for (const [gRef, iActive] of Object.entries(groupRef)) {
+          groupRef[gRef] = {hidden: !iActive};
         }
-        return false;
-    },
-    toggleDarkness: async () => {
-        const targetDarkLevel = canvas.lighting.darknessLevel === 1 ? 0 : 1;
-        return canvas.lighting.animateDarkness(targetDarkLevel, 20000).then(() => {
-            if (game.user.isGM) {
-                return canvas.scene.update({darkness: targetDarkLevel});
-            }
-            return false;
-        });
-    },
-    setLights: async (groupRef, updateData) => {
-        if (game.user.isGM) {
-            return canvas.scene.updateEmbeddedDocuments("AmbientLight", parseLightUpdateData(groupRef, updateData));
-        }
-        return false;
-    },
-    setSounds: async (soundName, updateData) => {
-        if (game.user.isGM) {
-            return canvas.scene.updateEmbeddedDocuments("AmbientSound", parseSoundUpdateData(soundName, updateData));
-        }
-        return false;
+        updateData.push(...parseLightUpdateData(groupRef));
+      } else {
+        updateData.push(...parseLightUpdateData(groupRef, {hidden: !isActive}));
+      }
+      return canvas.scene.updateEmbeddedDocuments("AmbientLight", updateData);
     }
+    return false;
+  },
+  toggleDarkness: async () => {
+    const targetDarkLevel = canvas.lighting.darknessLevel === 1 ? 0 : 1;
+    return canvas.lighting.animateDarkness(targetDarkLevel, 20000).then(() => {
+      if (game.user.isGM) {
+        return canvas.scene.update({darkness: targetDarkLevel});
+      }
+      return false;
+    });
+  },
+  setLights: async (groupRef, updateData) => {
+    if (game.user.isGM) {
+      return canvas.scene.updateEmbeddedDocuments("AmbientLight", parseLightUpdateData(groupRef, updateData));
+    }
+    return false;
+  },
+  setSounds: async (soundName, updateData) => {
+    if (game.user.isGM) {
+      return canvas.scene.updateEmbeddedDocuments("AmbientSound", parseSoundUpdateData(soundName, updateData));
+    }
+    return false;
+  },
+  scaleLightGroup: async (groupRef, {bright, dim, intensity, radius}) => {
+    if (game.user.isGM) {
+      return canvas.scene.updateEmbeddedDocuments(
+        "AmbientLight",
+        parseLightUpdateData(groupRef,
+                             canvas.scene.getEmbeddedCollection("AmbientLight")
+                               .filter((light) => new RegExp(groupRef, "ui").test(light.getFlag("alienrpgoverrides", "group")))
+                               .map((groupLight) => ({
+                                 _id: groupLight.id,
+                                 bright: Math.max(groupLight.data.bright, groupLight.data.dim) * (bright ?? 1),
+                                 dim: Math.max(groupLight.data.bright, groupLight.data.dim) * (dim ?? 1),
+                                 tintAlpha: groupLight.data.tintAlpha * (intensity ?? 1),
+                                 radius: groupLight.data.radius * (radius ?? 1)
+                               })))
+      );
+    }
+    return false;
+  }
 }))();
+
+
+let multBRIGHT = 0,
+    multDIM = 1.5;
+
+canvas.scene.updateEmbeddedDocuments("AmbientLight", Array.from(canvas.scene.getEmbeddedCollection("AmbientLight")
+  /* .filter((light) => new RegExp("SHIPLIGHTS-AWAKE", "ui").test(light.getFlag("alienrpgoverrides", "group"))) */
+  .map((groupLight) => {
+    const radius = groupLight.getFlag("alienrpgoverrides", "radius") ?? Math.max(groupLight.data.bright, groupLight.data.dim);
+    const bright = radius * multBRIGHT;
+    const dim = radius * multDIM;
+    return {
+      "_id": groupLight.id,
+      bright,
+      dim,
+      "data.flag.alienrpgoverrides.radius": radius
+    };
+  })));
+
+  */
